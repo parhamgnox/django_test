2a96646eef520757168eae0eb2675e64e4c4a6bc
parhamgnox <137355548+parhamgnox@users.noreply.github.com>
2023-07-16 03:19:07 -0700

    this is for training

--- config/settings.py
+++ config/settings.py
@@ -118,6 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+STATIC_ROOT = BASE_DIR/'/static'
+MEDIA_ROOT = BASE_DIR/'/media'
+
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
